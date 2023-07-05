c26b9cfbfeedefbc66ef0179001cb48c7cfb6f06
LusCard <luquiscardenas@gmail.com>
2023-07-05 15:08:38 -0300

    archivo database.js hecho

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,5 +1,5 @@
 // TODO: Crear modelo de datos de Reserva
-const { DataTypes, sequelize } = require("../database/database");
+const { DataTypes, sequelize } = require("../data/database");
 
 const Reserva = sequelize.define(
   "Reserva",
